a051c7c194864dc1fd863a12f1253232c9a1c133
Dom Heinzeller <dom.heinzeller@icloud.com>
2022-08-17 16:47:37 -0600

    Update cmake/fms_compiler_flags.cmake and cmake/compiler_flags_Intel_Fortran.cmake to match NOAA-GFDL code

--- cmake/fms_compiler_flags.cmake
+++ cmake/fms_compiler_flags.cmake
@@ -1,70 +1,25 @@
-# (C) Copyright 2018-2020 UCAR.
-#
-# This software is licensed under the terms of the Apache Licence Version 2.0
-# which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+################################################################################
+# Fortran
+################################################################################
 
-
-# Standard FMS compiler definitions
-# ---------------------------------
-add_definitions( -Duse_libMPI -DSPMD -Duse_netCDF -Duse_LARGEFILE -DGFS_PHYS -DINTERNAL_FILE_NML )
-
-# Special cases
-# -------------
-if( CMAKE_Fortran_COMPILER_ID MATCHES "GNU" OR CMAKE_Fortran_COMPILER_ID MATCHES "Clang")
-
-  set( CMAKE_Fortran_FLAGS "${CMAKE_Fortran_FLAGS} -ffree-line-length-none -fdec -fno-range-check ")
-
-elseif( CMAKE_Fortran_COMPILER_ID MATCHES "Intel" )
-
-  include( compiler_flags_Intel_Fortran )
-
-endif()
-
-if( APPLE )
-  add_definitions( -D__APPLE__ )
-endif()
-
-# Option to compile FMS in single or double precision
-# --------------------------------------------------------
-if (FV3LM_PRECISION MATCHES "DOUBLE" OR NOT FV3LM_PRECISION)
-
-  # Add double precision compilation flags
-  if( CMAKE_Fortran_COMPILER_ID MATCHES "Clang" )
-
-    set( CMAKE_Fortran_FLAGS "${CMAKE_Fortran_FLAGS} -fdefault-real-8 -fdefault-double-8 ")
-
-  elseif( CMAKE_Fortran_COMPILER_ID MATCHES "Cray" )
-
-    set( CMAKE_Fortran_FLAGS "${CMAKE_Fortran_FLAGS} --sreal64 ")
-
-  elseif( CMAKE_Fortran_COMPILER_ID MATCHES "GNU" )
-
-    set( CMAKE_Fortran_FLAGS "${CMAKE_Fortran_FLAGS} -fdefault-real-8 -fdefault-double-8 ")
-
-  elseif( CMAKE_Fortran_COMPILER_ID MATCHES "Intel" )
-
-    set( CMAKE_Fortran_FLAGS "${CMAKE_Fortran_FLAGS} -r8")
-
-  elseif( CMAKE_Fortran_COMPILER_ID MATCHES "PGI" )
-
-    set( CMAKE_Fortran_FLAGS "${CMAKE_Fortran_FLAGS} -r8")
-
-  elseif( CMAKE_Fortran_COMPILER_ID MATCHES "NVHPC" )
-
-    set( CMAKE_Fortran_FLAGS "${CMAKE_Fortran_FLAGS} -r8")
-
-  elseif( CMAKE_Fortran_COMPILER_ID MATCHES "XL" )
-
-    set( CMAKE_Fortran_FLAGS "${CMAKE_Fortran_FLAGS} -qdpc")
-
-  else()
-
-    message( FATAL "Fortran compiler with ID ${CMAKE_CXX_COMPILER_ID} does not have double precision flags set")
-
-  endif()
-
+if(CMAKE_Fortran_COMPILER_ID MATCHES "GNU")
+  include(compiler_flags_GNU_Fortran)
+elseif(CMAKE_Fortran_COMPILER_ID MATCHES "Intel")
+  include(compiler_flags_Intel_Fortran)
 else()
+  message(WARNING "Fortran compiler with ID ${CMAKE_Fortran_COMPILER_ID} will be used with CMake default options")
+endif()
 
-  add_definitions( -DOVERLOAD_R4 -DOVERLOAD_R8 )
+################################################################################
+# C
+################################################################################
 
+if(CMAKE_C_COMPILER_ID MATCHES "GNU")
+  include(compiler_flags_GNU_C)
+elseif(CMAKE_C_COMPILER_ID MATCHES "Intel")
+  include(compiler_flags_Intel_C)
+elseif(CMAKE_C_COMPILER_ID MATCHES "Clang")
+  include(compiler_flags_Clang_C)
+else()
+  message(WARNING "C compiler with ID ${CMAKE_C_COMPILER_ID} will be used with CMake default options")
 endif()
